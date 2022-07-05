06c4dd91ae8677da2cd9386c2cebb8cb431a0412
Sharanya Gowda <gowda.sharanya310@gmail.com>
2022-07-05 00:13:45 -0400

    Update cachematrix.R
    Added comments across the code.

--- cachematrix.R
+++ cachematrix.R
@@ -1,36 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a matrix 
+## x is the input to the matrix
+## matrix inverse value "mi" is set to null
+## every reference of "mean" from the example code is changed to "matrixinverse"
 
 makeCacheMatrix <- function(x = matrix()) {
-m<-NULL
+mi<-NULL
 set <- function(y)
 {
   x <<- y
-  m <<- NULL
+  mi <<- NULL
 }
 get <- function() x
-setmatrix <- function(matrix) m <<- matrix
-getmatrix <- function() m
+setmatrixinverse <- function(matrixinverse) mi <<- matrixinverse
+getmatrixinverse <- function() mi
 list(set = set, get = get,
-     setmatrix = setmatrix,
-     getmatrix = getmatrix)
+     setmatrixinverse = setmatrixinverse,
+     getmatrixinverse = getmatrixinverse)
 }
 
-
-## Write a short comment describing this function
+## every reference of "mean" from the example code is changed to "matrixinverse"
+##every reference of "m" from the example code is changed to "mi"
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-  m <- x$getmatrix()
-  if(!is.null(m)) {
-    message("getting cached data")
-    return(m)
+        ## Returns a matrix that is the inverse of 'x'
+  mi <- x$getmatrixinverse()
+  if(!is.null(mi)) {
+    message("cache the inverse of the matrix")
+    return(mi)
   }
   data <- x$get()
-  m <- matrix(data, ...)
-  x$setmatrix(m)
-  m
-  
+  mi <- matrixinverse(data, ...)
+  x$setmatrixinverse(mi)
+  mi
 }
